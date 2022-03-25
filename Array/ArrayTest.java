721f1475999f37a29081f133ea540c046af12d89
凉风有信 <3042146237@qq.com>
2022-03-25 16:25:50 +0800

    init

---
+++ Array/ArrayTest.java
@@ -0,0 +1,10 @@
+public class ArrayTest {
+	public static void main(String [] args) {
+		int [] arr1 = new int[10]; 
+		int arr2 [] = new int[20];
+
+		System.out.println(arr1.length);
+		System.out.println(arr2.length);
+	}
+}
+
